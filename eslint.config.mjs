54100d8e6ebf547c0fe7d557752d1224776ddb76
Luke McDonald <thelukemcdonald@gmail.com>
2025-10-01 07:44:40 -0500

    feat: add perfectionist eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import perfectionistPlugin from "eslint-plugin-perfectionist";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,6 +20,83 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+  },
+  {
+    plugins: {
+      perfectionist: perfectionistPlugin,
+    },
+    rules: {
+      // Turn off rules that conflict with Perfectionist
+      "import/order": 'off',
+      "react/jsx-sort-props": 'off',
+      "sort-imports": 'off',
+      "sort-keys": 'off',
+
+      // Perfectionist rules
+      "perfectionist/sort-objects": [
+        "error",
+        {
+          order: "asc",
+          type: "alphabetical",
+          partitionByComment: true,
+          partitionByNewLine: true,
+        },
+      ],
+      "perfectionist/sort-interfaces": [
+        "error",
+        {
+          order: "asc",
+          type: "alphabetical",
+        },
+      ],
+      "perfectionist/sort-object-types": [
+        "error",
+        {
+          order: "asc",
+          type: "alphabetical",
+        },
+      ],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          customGroups: {
+            value: {
+              internal: ['^@/.*'],
+              react: ['^react$', '^react-.+'],
+            },
+          },
+          groups: [
+            "type",
+            "react",
+            ["builtin", "external"],
+            "internal-type",
+            "internal",
+            ["parent-type", "sibling-type", "index-type"],
+            ["parent", "sibling", "index"],
+            "object",
+            "unknown",
+          ],
+          newlinesBetween: 'always',
+          order: "asc",
+          type: "alphabetical",
+        },
+      ],
+      // Sort named imports alphabetically
+      "perfectionist/sort-named-imports": [
+        "error",
+        {
+          order: "asc",
+          type: "alphabetical",
+        },
+      ],
+    },
+    settings: {
+      perfectionist: {
+        order: 'asc',
+        partitionByComment: true,
+        type: "natural",
+      },
+    },
   },
 ];
 
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  // plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 100,
+  proseWrap: 'always',
+  singleQuote: true,
+  trailingComma: 'all',
+  // tailwindFunctions: ['cn', 'cva'],
+}
