69cd3aaf260bd72542249d5ac04867cea18c1848
Luke McDonald <thelukemcdonald@gmail.com>
2025-10-11 20:14:04 -0500

    fix: remove favorites features

--- lib/features/favorites/hooks.ts
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useQuery } from 'convex/react';
-
-import { api } from '@/convex/_generated/api';
-
-export function useUserFavorites(limit?: number) {
-  return useQuery(api.users.listFavorites, { limit });
-}
--- lib/features/users/hooks.ts
+++ lib/features/users/hooks.ts
@@ -7,3 +7,7 @@ import { api } from '@/convex/_generated/api';
 export function useCurrentUser() {
   return useQuery(api.auth.getCurrentUser);
 }
+
+export function useUserFavorites(limit?: number) {
+  return useQuery(api.users.listFavorites, { limit });
+}
