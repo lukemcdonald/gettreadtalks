a4cd473698fd11b29e78b03e2e477e46d4f6f70f
Luke McDonald <thelukemcdonald@gmail.com>
2025-11-30 12:57:35 -0600

    refactor: simplify page header and center navigation

--- src/components/site-header/site-header.tsx
+++ src/components/site-header/site-header.tsx
@@ -1,7 +1,11 @@
 import { Container } from '@/components/container';
 import { Section } from '@/components/section';
+import { MobileNav } from '@/components/site-header/navigation/mobile-nav';
+import { PrimaryNav } from '@/components/site-header/navigation/primary-nav';
+import { SecondaryNav } from '@/components/site-header/navigation/secondary-nav';
 import { SiteBranding } from '@/components/site-header/site-branding';
 import { SiteNavigation } from '@/components/site-header/site-navigation';
+import { Separator } from '@/components/ui/separator';
 import { getCurrentUser } from '@/services/auth/server';
 
 export async function SiteHeader() {
@@ -15,11 +19,11 @@ export async function SiteHeader() {
       role="banner"
       spacing="sm"
     >
-      <Container>
-        <div className="flex flex-row items-center justify-between gap-2">
-          <SiteBranding />
-          <SiteNavigation initialUser={initialUser} />
-        </div>
+      <Container className="flex items-center justify-between gap-2">
+        <SiteBranding className="flex flex-1 justify-start" />
+        <PrimaryNav className="hidden justify-center md:flex lg:flex-1" />
+        <SecondaryNav className="flex justify-end lg:flex-1" initialUser={initialUser} />
+        <MobileNav className="md:hidden" />
       </Container>
     </Section>
   );
--- src/features/users/hooks/index.ts
+++ src/features/users/hooks/index.ts
@@ -1,5 +1,4 @@
 export { useCurrentUser } from './use-current-user';
 export { useFavoriteTalk } from './use-favorite-talk';
-export { useIsAdmin } from './use-is-admin';
 export { useIsTalkFavorited } from './use-is-talk-favorited';
 export { useUnfavoriteTalk } from './use-unfavorite-talk';
