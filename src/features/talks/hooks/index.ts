d281cc0d222379a536965fcac855382732377cd2
Luke McDonald <thelukemcdonald@gmail.com>
2025-12-07 05:28:22 -0600

    fix: remove unused hooks

--- src/features/talks/hooks/index.ts
+++ src/features/talks/hooks/index.ts
@@ -1,7 +1,3 @@
 export { useArchiveTalk } from './use-archive-talk';
 export { useCreateTalk } from './use-create-talk';
-export { usePaginatedTalks } from './use-paginated-talks';
-export { useTalkBySlug } from './use-talk-by-slug';
-export { useTalksBySpeaker } from './use-talks-by-speaker';
-export { useTalksWithSpeakers } from './use-talks-with-speakers';
 export { useUpdateTalk } from './use-update-talk';
