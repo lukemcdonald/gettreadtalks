b2cfa4d1ec46d1aa759af595f5bdce5d2fe40368
Luke McDonald <thelukemcdonald@gmail.com>
2026-01-08 20:26:53 -0600

    refactor: remove unused components and hooks

--- src/features/talks/hooks/use-create-talk.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { api } from '@/convex/_generated/api';
-import { useMutation } from '@/hooks';
-
-export function useCreateTalk() {
-  return useMutation(api.talks.createTalk);
-}
