c465c55b8f05411b1ed03d9722772f8487f04aff
Luke McDonald <thelukemcdonald@gmail.com>
2026-01-09 12:40:09 -0600

    feat: filter speakers page to show only speakers with published talks

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -1,10 +1,10 @@
 import { SpeakersContent } from '@/app/speakers/_components/speakers-content';
 import { SpeakersSidebar } from '@/app/speakers/_components/speakers-sidebar';
 import { SidebarLayout } from '@/components/layouts';
-import { getSpeakers } from '@/features/speakers';
+import { getSpeakersWithPublishedTalks } from '@/features/speakers';
 
 export default async function SpeakersPage() {
-  const { speakers } = await getSpeakers();
+  const { speakers } = await getSpeakersWithPublishedTalks();
 
   return (
     <SidebarLayout
--- src/features/speakers/queries/index.ts
+++ src/features/speakers/queries/index.ts
@@ -2,3 +2,4 @@ export { getFeaturedSpeakers } from './get-featured-speakers';
 export { getSpeakerBySlug } from './get-speaker-by-slug';
 export { getSpeakers } from './get-speakers';
 export { getSpeakersGrouped } from './get-speakers-grouped';
+export { getSpeakersWithPublishedTalks } from './get-speakers-with-published-talks';
