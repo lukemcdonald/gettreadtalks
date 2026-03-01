5e74f41ef9aca0b30abcef8833496919340db2c0
Luke McDonald <thelukemcdonald@gmail.com>
2026-02-28 21:27:57 -0600

    refactor: remove redundant comments from skeleton components

--- src/components/skeletons/media-card-skeleton.tsx
+++ src/components/skeletons/media-card-skeleton.tsx
@@ -8,12 +8,9 @@ interface MediaCardSkeletonProps {
 export function MediaCardSkeleton({ className }: MediaCardSkeletonProps) {
   return (
     <Card className={cn('flex-row gap-4 border-0 p-4', className)}>
-      {/* Avatar placeholder */}
       <Skeleton className="size-12 shrink-0 rounded-md" />
       <div className="flex-1 space-y-2">
-        {/* Title placeholder */}
         <Skeleton className="h-5 w-3/4" />
-        {/* Subtitle placeholder */}
         <Skeleton className="h-4 w-1/3" />
       </div>
     </Card>
